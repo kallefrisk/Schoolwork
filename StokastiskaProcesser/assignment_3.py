c5f5d57db208595a90e5ed8d6498c1d7915f85d2
kallefrisk <kallefrisk@live.se>
2025-12-15 16:58:05 +0100

    Renamed Stochastic Processes -> StochasticProcesses and redid Assignment_3.py

---
+++ StokastiskaProcesser/assignment_3.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import random
+
+lamd = 1
+mu = 1.2
+stop_time = 12
+queue = 0
+time = 0
+timestamps = [0]
+changes = [0]
+time_no_customers = 0
+
+while time < stop_time:
+    arrival_time = random.expovariate(lamd)
+    queue_time = random.expovariate(mu)
+    if queue == 0:
+        time_no_customers += min(arrival_time, queue_time)
+    if (arrival_time >= queue_time) and (queue == 0):
+        time += arrival_time
+        queue += 1
+        timestamps += [time]
+        changes += [queue]
+    elif arrival_time <= queue_time:
+        time += arrival_time
+        queue += 1
+        timestamps += [time]
+        changes += [queue]
+    else:
+        time += queue_time
+        queue -= 1
+        if queue < 0:
+            queue = 0
+        timestamps += [time]
+        changes += [queue]
+
+changes.pop(-1)
+
+plt.stairs(changes, timestamps)
+plt.xlabel('time')
+plt.ylabel('# of customers')
+plt.show()
