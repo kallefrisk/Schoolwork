5f90f4e7e318191f9109423fcc08c2bb2e8c930c
Karl Frisk <kf222rj@student.lnu.se>
2025-12-16 15:08:51 +0100

    Added a variable to controll what number of customers to track

--- Python/StochasticProcesses/assignment_3.py
+++ Python/StochasticProcesses/assignment_3.py
@@ -1,43 +1,57 @@
 import matplotlib.pyplot as plt
 import random
 
+def long_run_proportion(lamda, mu, n):
+    return (lamda/mu)**n*(1-lamda/mu)
+
+
 lamda = 1
-mu = 1.5
-stop_time = 12
+mu = 1.3
+stop_time = 100
 queue = 0
 time = 0
-time_no_customers = 0
+time_n_customers = 0
 timestamps = [0]
-changes = [0]
+queue_length = [0]
+n = 0
 
 while time < stop_time:
     arrival_time = random.expovariate(lamda)
     queue_time = random.expovariate(mu)
-    if queue == 0:
-        time_no_customers += min(arrival_time, queue_time)
-    # if (arrival_time >= queue_time) and (queue == 0):
+    if queue == n:
+        time_n_customers += min(arrival_time, queue_time)
+    # if (arrival_time >= queue_time) and (queue == n):
     #     time += arrival_time
     #     queue += 1
     #     timestamps += [time]
-    #     changes += [queue]
+    #     queue_length += [queue]
     if arrival_time <= queue_time:
         time += arrival_time
         queue += 1
         timestamps += [time]
-        changes += [queue]
+        queue_length += [queue]
     else:
         time += queue_time
         queue -= 1
         if queue < 0:
             queue = 0
         timestamps += [time]
-        changes += [queue]
+        queue_length += [queue]
 
-changes.pop(-1)
+queue_length.pop(-1)
+timestamps[-1] = stop_time
 
-plt.stairs(changes, timestamps)
+expected_value = 0
+for i in range(max(queue_length)):
+    expected_value += i * long_run_proportion(lamda, mu, i)
+
+print(expected_value)
+
+plt.stairs(queue_length, timestamps, label='customers')
+plt.plot(timestamps, [expected_value for _ in range(len(timestamps))], 'r-', label='mean')
 plt.xlabel('time')
 plt.ylabel('# of customers')
+plt.legend()
 plt.show()
 
-print(time_no_customers/timestamps[-1], 1-lamda/mu)
+print(time_n_customers/timestamps[-1], (lamda/mu)**n*(1-lamda/mu))
