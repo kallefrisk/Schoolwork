f80c2e0fcf7650e350f8e594d4b8287d7ccf8a6f
kallefrisk <kallefrisk@live.se>
2025-12-16 18:23:06 +0100

    Tried to implement the expected time to travel from state i to state j but not complete yet.

--- Python/StochasticProcesses/assignment_3.py
+++ Python/StochasticProcesses/assignment_3.py
@@ -1,42 +1,64 @@
 import matplotlib.pyplot as plt
 import random
 
+
 def long_run_proportion(lamda, mu, n):
     return (lamda/mu)**n*(1-lamda/mu)
 
 
+def expected_time_i(lamda, mu, i):
+    return (1 - (mu/lamda)**(i))/(lamda - mu)
+
+
 lamda = 1
 mu = 1.3
-stop_time = 100
+stop_time = 10000
 queue = 0
 time = 0
 time_n_customers = 0
+total_time_to_n_customers = 0
 timestamps = [0]
 queue_length = [0]
 n = 0
+from_k = 0
+to_n = 4
+coming_from_k = False
+times_arrived = 0
 
 while time < stop_time:
     arrival_time = random.expovariate(lamda)
     queue_time = random.expovariate(mu)
+
+    # Add time to count how long we spend in state n
     if queue == n:
         time_n_customers += min(arrival_time, queue_time)
-    # if (arrival_time >= queue_time) and (queue == n):
-    #     time += arrival_time
-    #     queue += 1
-    #     timestamps += [time]
-    #     queue_length += [queue]
+
+    # Start the timer to count time from state k to state n
+    if queue == from_k and not coming_from_k:
+        time_to_n_customers = 0
+        coming_from_k = True
+
+    # Record time taken from state k to state n
+    elif queue == to_n and coming_from_k:
+        total_time_to_n_customers += time_to_n_customers
+        times_arrived += 1
+        coming_from_k = False
+
+    # Check which event happens first and update variables accordingly
     if arrival_time <= queue_time:
-        time += arrival_time
         queue += 1
-        timestamps += [time]
-        queue_length += [queue]
     else:
-        time += queue_time
         queue -= 1
-        if queue < 0:
-            queue = 0
-        timestamps += [time]
-        queue_length += [queue]
+
+    # Make sure queue stays non-negative
+    if queue < 0:
+        queue = 0
+
+    shorter_time = min(arrival_time, queue_time)
+    time += shorter_time
+    timestamps += [time]
+    queue_length += [queue]
+    time_to_n_customers += shorter_time
 
 queue_length.pop(-1)
 timestamps[-1] = stop_time
@@ -45,13 +67,14 @@ expected_value = 0
 for i in range(max(queue_length)):
     expected_value += i * long_run_proportion(lamda, mu, i)
 
-print(expected_value)
+expected_value_vector = [expected_value for _ in range(len(timestamps))]
+
+print(f'\nFraction of time when {n} customers were in the queue: {time_n_customers/timestamps[-1]}. Expected fraction: {(lamda/mu)**n*(1-lamda/mu)}')
+print(f'\nAverage time to reach {to_n} customers from {from_k} customers: {total_time_to_n_customers/times_arrived}. Expected time: {expected_time_i(lamda, mu, to_n)}\n')
 
 plt.stairs(queue_length, timestamps, label='customers')
-plt.plot(timestamps, [expected_value for _ in range(len(timestamps))], 'r-', label='mean')
+plt.plot(timestamps, expected_value_vector, 'r-', label='mean')
 plt.xlabel('time')
 plt.ylabel('# of customers')
 plt.legend()
 plt.show()
-
-print(time_n_customers/timestamps[-1], (lamda/mu)**n*(1-lamda/mu))
