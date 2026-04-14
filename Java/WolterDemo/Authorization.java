41976c45f5a2ac3c7f9b6dcbc5c6839ce015a24c
kallefrisk <kallefrisk@live.se>
2026-04-15 00:50:04 +0200

    Finished Authorization.java.

--- Java/WolterDemo/Authorization.java
+++ Java/WolterDemo/Authorization.java
@@ -12,7 +12,7 @@ public class Authorization {
    * @param pass3 password 3
    * @return true/false
    */
-  boolean isAuthorized(int pass1, int pass2, int pass3) {
+  static boolean isAuthorized(int pass1, int pass2, int pass3) {
     
     int truePass1 = 1;
     int truePass2 = 12;
@@ -24,7 +24,22 @@ public class Authorization {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
 
+    System.out.print("\nPlease enter the first password: ");
+    int pass1 = s.nextInt();
 
+    System.out.print("\nPlease enter the second password: ");
+    int pass2 = s.nextInt();
+
+    System.out.print("\nPlease enter the third password: ");
+    int pass3 = s.nextInt();
+
+    boolean authorized = isAuthorized(pass1, pass2, pass3);
+
+    if (authorized) {
+      System.out.println("\nYou ARE authorized!\n");
+    } else {
+      System.out.println("\nYou ARE NOT authorized!\n");
+    }
 
     s.close();
   }
