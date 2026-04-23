18466375dce2480a067b7bc1db90cc71b9d44dae
kallefrisk <kallefrisk@live.se>
2026-04-23 19:17:05 +0200

    Fixed amount of rows to plot.

--- Python/BayesianMethods/plotting_pdfs/beta_subplots.py
+++ Python/BayesianMethods/plotting_pdfs/beta_subplots.py
@@ -16,7 +16,7 @@ plt.figure()
 for i, values in enumerate(parameters.values()):
     alpha = values[0]
     beta = values[1]
-    plt.subplot(len(parameters) // 5 + 1, 3, i + 1)
+    plt.subplot(-(-len(parameters) // 5), 5, i + 1)
     y = stats.beta.pdf(x, alpha, beta)
     plt.plot(x, y)
     plt.title(f'α={alpha}, β={beta}')
